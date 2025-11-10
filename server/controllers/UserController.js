ef0f1c66a246edc799546afa1d1a5841e2955851
Harshit Keshari <harshitkeshari9621@gmail.com>
2025-11-11 00:10:12 +0530

    first commit

---
+++ server/controllers/UserController.js
@@ -0,0 +1,6 @@
+// API controller function to manage clerk user with database 
+// http://localhost:4000/api/user/webhooks -> this si webhooks end point 
+
+const clerkWebhooks = async(req,res) =>{
+    
+}
---
+++ server/server.js
@@ -0,0 +1,25 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import connectDB from './config/mongoDb.js';
+
+
+
+
+// App config 
+const PORT=process.env.PORT || 4000;
+
+const app =express()
+await connectDB()
+
+// Middlewares 
+app.use(express.json()) // for parsing
+app.use(cors()) // to connect client running on different port
+
+// API routes
+
+app.get('/',(req,res)=>{res.send("API Working")})
+
+app.listen(PORT,()=>{
+    console.log(`Running on PORT: http://localhost:${PORT} `)
+});
