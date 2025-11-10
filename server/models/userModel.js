b7bfa8119d30008cd0692bfa7718c2055c9cf1a7
Harshit Keshari <harshitkeshari9621@gmail.com>
2025-11-11 01:12:35 +0530

    frtgyhuj

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -9,6 +9,6 @@ const userSchema = new mongoose.Schema({
     creditBalance:{type:Number, default:5} 
 })
 
-const userModel = mongoose.model.user || mongoose.model("user",userSchema)
+const userModel = mongoose.model.user || mongoose.model("users",userSchema)
 
 export default userModel;
