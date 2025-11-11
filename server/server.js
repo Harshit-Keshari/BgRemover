8da63055158660bdb64f2cd55d96f326f9e628ba
Harshit Keshari <harshitkeshari9621@gmail.com>
2025-11-11 18:09:57 +0530

    comment

--- server/server.js
+++ server/server.js
@@ -13,10 +13,10 @@ app.use(cors())
 app.get('/', (req, res) => res.send('API Working ✅'))
 app.use('/api/user', userRouter)
 
-app.post('/api/webhook', express.json(), async (req, res) => {
-  console.log('Webhook received:', req.body);
-  res.status(200).send('ok');
-});
+// app.post('/api/webhook', express.json(), async (req, res) => {
+//   console.log('Webhook received:', req.body);
+//   res.status(200).send('ok');
+// });
 
 
 // Connect DB safely
