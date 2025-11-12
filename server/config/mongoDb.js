a1ec2394e8a49dd665a0f5fc7e8af45166bb1a40
Harshit Keshari <harshitkeshari9621@gmail.com>
2025-11-12 23:49:52 +0530

    sdf

--- server/config/mongoDb.js
+++ server/config/mongoDb.js
@@ -4,7 +4,7 @@ const connectDB = async()=>{
     mongoose.connection.on('connected',()=>{
         console.log("Database Connected");
     })
-    await mongoose.connect(`${process.env.MONGODB_URI}/Bg-remover`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/bgRemoval`)
 }
 
 export default connectDB;
--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -4,18 +4,18 @@
 import { Webhook } from "svix"
 import userModel from "../models/userModel.js"
 
-export const clerkWebhooks = async (req, res) => {
+const clerkWebhooks = async (req, res) => {
   try {
     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
     // Verify webhook
-    await whook.verify(req.body.toString(), {
+    await whook.verify(JSON.stringify(req.body), {
       "svix-id": req.headers["svix-id"],
       "svix-timestamp": req.headers["svix-timestamp"],
       "svix-signature": req.headers["svix-signature"],
     })
 
-    const { data, type } = JSON.parse(req.body)
+    const { data, type } = req.body
 
     switch (type) {
       case "user.created": {
@@ -24,13 +24,13 @@ export const clerkWebhooks = async (req, res) => {
           email: data.email_addresses[0].email_address,
           photo: data.image_url,
           firstName: data.first_name,
-          lastName: data.last_name,
+          lastName: data.last_name
         }
 
         await userModel.create(userData)
         console.log("✅ User created:", userData.email)
         res.json({ success: true })
-        break
+        break;
       }
 
       case "user.updated": {
@@ -38,27 +38,30 @@ export const clerkWebhooks = async (req, res) => {
           email: data.email_addresses[0].email_address,
           photo: data.image_url,
           firstName: data.first_name,
-          lastName: data.last_name,
+          lastName: data.last_name
         }
 
         await userModel.findOneAndUpdate({ clerkId: data.id }, userData)
         console.log("🟡 User updated:", userData.email)
         res.json({ success: true })
-        break
+        break;
       }
 
       case "user.deleted": {
         await userModel.findOneAndDelete({ clerkId: data.id })
         console.log("🗑️ User deleted:", data.id)
         res.json({ success: true })
-        break
+        break;
       }
 
       default:
         res.json({ received: true })
+        break;
     }
   } catch (error) {
     console.error("❌ Webhook Error:", error.message)
     res.status(400).json({ success: false, message: error.message })
   }
 }
+
+export {clerkWebhooks}
